5ecd3db01df04fd2cc1ff96a886ffb2f4d86c2bf
dmitryboz21 <dmitryboz21@yandex.com>
2022-07-12 18:59:57 +0300

    123

--- src/js/partials/map_old_lite.js
+++ src/js/partials/map_old_lite.js
@@ -1,8 +1,6 @@
-var homepageMapObject = -1;
 
 
-
-var mapDarkStyles = [
+var mapLightStyles = [
 	{
 		"featureType": "all",
 		"elementType": "labels.text.fill",
@@ -11,7 +9,7 @@ var mapDarkStyles = [
 				"saturation": 36
 			},
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 40
@@ -26,7 +24,7 @@ var mapDarkStyles = [
 				"visibility": "on"
 			},
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 16
@@ -47,7 +45,7 @@ var mapDarkStyles = [
 		"elementType": "geometry.fill",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 20
@@ -59,7 +57,7 @@ var mapDarkStyles = [
 		"elementType": "geometry.stroke",
 		"stylers": [
 			{
-				"lightness": 17
+				"lightness": 20
 			},
 			{
 				"weight": 1.2
@@ -74,7 +72,7 @@ var mapDarkStyles = [
 				"visibility": "on"
 			},
 			{
-				"color": "#a0ab7b"
+				"color": "#4F542D"
 			}
 		]
 	},
@@ -89,7 +87,7 @@ var mapDarkStyles = [
 				"weight": "1.00"
 			},
 			{
-				"color": "#a0ab7b"
+				"color": "#4F542D"
 			}
 		]
 	},
@@ -98,7 +96,7 @@ var mapDarkStyles = [
 		"elementType": "geometry",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 20
@@ -110,7 +108,7 @@ var mapDarkStyles = [
 		"elementType": "geometry",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 21
@@ -122,7 +120,7 @@ var mapDarkStyles = [
 		"elementType": "geometry.fill",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 17
@@ -134,7 +132,7 @@ var mapDarkStyles = [
 		"elementType": "geometry.stroke",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 29
@@ -149,7 +147,7 @@ var mapDarkStyles = [
 		"elementType": "geometry",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 18
@@ -161,7 +159,7 @@ var mapDarkStyles = [
 		"elementType": "geometry",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 16
@@ -173,7 +171,7 @@ var mapDarkStyles = [
 		"elementType": "geometry",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#F0F0ED"
 			},
 			{
 				"lightness": 19
@@ -185,7 +183,7 @@ var mapDarkStyles = [
 		"elementType": "geometry",
 		"stylers": [
 			{
-				"color": "#000000"
+				"color": "#4F542D"
 			},
 			{
 				"lightness": 17
@@ -200,12 +198,11 @@ var mapDarkStyles = [
 				"visibility": "on"
 			},
 			{
-				"color": "#151814"
+				"color": "#F0F0ED"
 			}
 		]
 	}
 ];
-
 
 var mapLightStyles = [
     {
@@ -226,47 +223,6 @@ var mapLightStyles = [
             }
         ]
     },
-
-
-	{
-		"featureType": "administrative",
-		"elementType": "geometry.stroke",
-		"stylers": [
-			{
-				"lightness": 20
-			},
-			{
-				"weight": 1.2
-			}
-		]
-	},
-	{
-		"featureType": "administrative.country",
-		"elementType": "geometry.stroke",
-		"stylers": [
-			{
-				"visibility": "on"
-			},
-			{
-				"color": "#4F542D"
-			}
-		]
-	},
-	{
-		"featureType": "administrative.province",
-		"elementType": "geometry.stroke",
-		"stylers": [
-			{
-				"visibility": "on"
-			},
-			{
-				"weight": "1.00"
-			},
-			{
-				"color": "#4F542D"
-			}
-		]
-	},
     {
         "featureType": "all",
         "elementType": "labels.text",
@@ -418,87 +374,3 @@ var mapLightStyles = [
         ]
     }
 ];
-
-
-
-var mapStyles = {
-	'color-theme-dark':mapDarkStyles,
-	'color-theme-light':mapLightStyles
-};
-
-
-window.addEventListener('load', () => {
-	const mapMain = document.getElementById('map');
-	const mapContacts = document.getElementById('map-contacts');
-	const locationsMain = [
-		{ lat: 62.037112, lng: 129.729814 },
-		/*{ lat: 62.025972, lng: 129.709701 },
-		{ lat: 62.026514, lng: 129.741825 },
-		{ lat: 62.018559, lng: 129.717276 },*/
-		{ lat: 56.321337, lng: 43.994564 },
-		/*{ lat: 56.314863, lng: 44.030613 },
-		{ lat: 56.294864, lng: 44.019455 },
-		{ lat: 56.310769, lng: 44.002289 },
-		{ lat: 56.325525, lng: 44.018254 },
-		{ lat: 56.311376, lng: 44.022641 },*/
-		//{ lat: 55.754910, lng: 37.618727}// не заыбть убрать
-	]
-	const locationsContacts = [
-		{ lat: 55.768976, lng: 37.670347 }
-	]
-
-	function initMap(el, lat, lng, zoom, locations) {
-
-		colorThemeName='color-theme-dark';
-		if (localStorage.hasOwnProperty('colorThemeName')) {
-			let lsThemeName = localStorage.getItem('colorThemeName');
-			if(lsThemeName && lsThemeName.length>0){
-				colorThemeName=lsThemeName;
-			}
-		}
-
-		const options = {
-			center: { lat, lng },
-			zoom,
-			disableDefaultUI: true,
-			styles: mapStyles[colorThemeName]
-
-		};
-
-		homepageMapObject = new google.maps.Map(el, options);
-
-		const markers = locations.map((position) => {
-			const marker = new google.maps.Marker({
-				position,
-				map: homepageMapObject,
-				icon: {
-					url: './images/map_icon_v2.png',
-					scaledSize:{
-						height: 92,
-						width: 74
-					},
-					anchor: new google.maps.Point(37, 63),
-				},
-			});
-
-			return marker;
-		});
-
-		const markerCluster = new markerClusterer.MarkerClusterer(homepageMapObject, markers, {
-			styles: [{
-				url: './images/map_icon_v2t.png',
-				textColor: 'green',
-				height: 92,
-				width: 74
-			}]
-		});
-	}
-
-	if (mapMain) {
-		initMap(mapMain, 65.061313, 101.477485, 3, locationsMain);
-	}
-
-	if (mapContacts) {
-		initMap(mapContacts, 55.768741, 37.672267, 17, locationsContacts);
-	}
-});
